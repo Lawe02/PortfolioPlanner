6995c4e8d4cf2ba4575d96ef649f4fb58cc0e9fa
Lawe02 <89772680+Lawe02@users.noreply.github.com>
2024-11-18 00:01:58 +0100

    added createPlanrequestDto

--- src/main/java/org/example/stockapi/Dto/req/CreatePlanRequestDto.java
+++ src/main/java/org/example/stockapi/Dto/req/CreatePlanRequestDto.java
@@ -1,4 +1,6 @@
 package org.example.stockapi.Dto.req;
 
-public record CreatePlanRequestDto(String name, String description, String userName) {
+import java.util.List;
+
+public record CreatePlanRequestDto(String name, String description, String userName, List<PlanStockRequestDto> stockPlans) {
 }
--- src/main/java/org/example/stockapi/service/PlanSerivce.java
+++ src/main/java/org/example/stockapi/service/PlanSerivce.java
@@ -1,19 +1,28 @@
 package org.example.stockapi.service;
 
+import org.example.stockapi.Dto.req.CreatePlanRequestDto;
+import org.example.stockapi.model.AppUser;
 import org.example.stockapi.model.Plan;
+import org.example.stockapi.repository.AppUserRepository;
 import org.example.stockapi.repository.PlanRepository;
+import org.example.stockapi.repository.StockRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PlanSerivce {
 
     private final PlanRepository planRepository;
+    private final AppUserRepository appUserRepository;
+    private final StockRepository stockRepository;
 
-    public PlanSerivce(PlanRepository planRepository) {
+    public PlanSerivce(PlanRepository planRepository, AppUserRepository appUserRepository, StockRepository stockRepository) {
         this.planRepository = planRepository;
+        this.appUserRepository = appUserRepository;
+        this.stockRepository = stockRepository;
     }
 
-    public void CreatePlan(Plan plan) {
-        planRepository.save(plan);
+    public void CreatePlan(CreatePlanRequestDto createPlanRequestDto) {
+
+
     }
 }
