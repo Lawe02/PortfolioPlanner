415f93dbda47ea850f45b3a971c07808493c1591
Lawe02 <89772680+Lawe02@users.noreply.github.com>
2024-11-17 22:29:28 +0100

    added repoistory annotation to appuserrepo

--- src/main/java/org/example/stockapi/repository/AppUserRepository.java
+++ src/main/java/org/example/stockapi/repository/AppUserRepository.java
@@ -2,7 +2,9 @@ package org.example.stockapi.repository;
 
 import org.example.stockapi.model.AppUser;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AppUserRepository extends ListCrudRepository<AppUser, Long> {
     AppUser findByUsername(String username);
 }
