78caebf6b6cd3e5c536ef62251005d8daff482dc
Lawe02 <89772680+Lawe02@users.noreply.github.com>
2024-11-17 22:31:00 +0100

    added repo field and constructor to planservice

--- src/main/java/org/example/stockapi/service/PlanSerivce.java
+++ src/main/java/org/example/stockapi/service/PlanSerivce.java
@@ -1,7 +1,14 @@
 package org.example.stockapi.service;
 
+import org.example.stockapi.repository.PlanRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PlanSerivce {
+
+    private final PlanRepository planRepository;
+
+    public PlanSerivce(PlanRepository planRepository) {
+        this.planRepository = planRepository;
+    }
 }
