9776ffd6b52ce02fee07197ce577da35c2c2bd74
Lawe02 <89772680+Lawe02@users.noreply.github.com>
2024-11-17 22:20:58 +0100

    added constructor and repo field to userService

--- src/main/java/org/example/stockapi/service/AppUserService.java
+++ src/main/java/org/example/stockapi/service/AppUserService.java
@@ -1,4 +1,14 @@
 package org.example.stockapi.service;
 
+import org.example.stockapi.repository.AppUserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AppUserService {
+
+    private final AppUserRepository appUserRepository;
+
+    public AppUserService(AppUserRepository appUserRepository) {
+        this.appUserRepository = appUserRepository;
+    }
 }
