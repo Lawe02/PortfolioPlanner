7b284aec01d397e61eeb6f6b46d99432b0d626f3
Lawe02 <89772680+Lawe02@users.noreply.github.com>
2024-12-21 01:29:11 +0100

    auth configured and works

--- src/main/java/org/example/stockapi/config/SecurityConfig.java
+++ src/main/java/org/example/stockapi/config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package org.example.stockapi.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
